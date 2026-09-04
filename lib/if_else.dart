c634a21c00ef272c6f5b406ec879d497e5e080a2
mhfzhrmzy <mhfzhrmzy@gmail.com>
2026-09-04 09:25:17 +0700

    nitip kode if if else switch dart

---
+++ lib/if_else.dart
@@ -0,0 +1,9 @@
+void main() {
+  int nilai = 60;
+
+  if (nilai >= 75) {
+    print("Lulus");
+  } else {
+    print("Tidak Lulus");
+  }
+}
---
+++ lib/switchcase.dart
@@ -0,0 +1,17 @@
+void main() {
+  int pilihan = 2;
+
+  switch (pilihan) {
+    case 1:
+      print("Senin");
+      break;
+    case 2:
+      print("Selasa");
+      break;
+    case 3:
+      print("Rabu");
+      break;
+    default:
+      print("Pilihan tidak tersedia");
+  }
+}
